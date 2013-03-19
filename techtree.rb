5d4645c57622ef79e6c786561e03a417a51e2793
Krzysztof Zych <krzysztof.zych@connectmedica.com>
2013-03-19 20:20:48 +0100

    todo o wykluczaniu

--- techtree.rb
+++ techtree.rb
@@ -7,6 +7,7 @@ Bundler.require
 require 'delegate'
 require 'yaml'
 require 'set'
+require 'pry'
 
 autoload :Item, './lib/item'
 autoload :Craft, './lib/craft'
@@ -25,15 +26,23 @@ DB.fixup_pending
 DB.dump_graph File.open('techtree.dot', 'w')
 
 def solve *names
+  if names.last.is_a? Hash
+    options = names.pop 
+  end
+  # TODO: wykluczanie maszyn - jeśli odpowiednia opcja podana,
+  # podmienić itemresolvera na używający craftresolvera wykluczającego podane maszyny
+  # analogicznie: wykluczanie clusterów - podmieniamy itemresolver na taki
+  # który nie znajdzie itemków z zakazanego clustera
   solutions = names.map do |name|
     if name =~ /(.+?)\*(\d+)/
       [$1, $2.to_i]
     else
       [name, 1]
     end
-  end.map { |name, count| ItemResolver.new(DB.find(name), count).resolve }
+  end
+  .map { |name, count| ItemResolver.new(DB.find(name), count).resolve }
   Solver.new(solutions).solve.describe
 end
 
-solve 'copper cable*1'
+solve 'molten redstone*1000'
 binding.pry
