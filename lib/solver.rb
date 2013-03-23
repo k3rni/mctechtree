68e218aba836908113454db990b33ea633d5df23
Krzysztof Zych <krzysztof.m.zych@gmail.com>
2013-03-23 19:30:15 +0100

    solver po nowemu atakujemy - craft requesty i stash

--- lib/solver.rb
+++ lib/solver.rb
@@ -1,11 +1,12 @@
 require 'ostruct'
 
 class Solver
-    attr_accessor :raw, :crafts, :craft_seq
+    attr_accessor :raw, :crafts, :craft_seq, :stash
 
     def initialize(solutions)
         @solutions = solutions
         @crafts = Hash.new { |h, key| h[key] = 0 }
+        @stash = Hash.new { |h, key| h[key] = 0 }
         @craft_seq = Hash.new { |h, key| h[key] = 0 }
         @raw = Hash.new { |h, key| h[key] = 0 }
     end
@@ -17,18 +18,49 @@ class Solver
     end
 
     def get depth, mul, count, item
-        # puts "#{' '*depth}GET #{mul} #{count} #{item}"
+        puts "#{' '*depth}GET[#{item}] += #{mul} * #{count}"
         @raw[item] += (mul * count)
     end
 
+    # TODO: 
+    # 1. craft sprawdza ile jest resulta w stashu
+    # 2. jeśli wystarczająco, odejmuje ze stasha i nie craftuje
+    # 3. jeśli niewystarczająco, craftuje tyle ile trzeba i dodaje do stasha
     def craft depth, mul, count, tree
-        # puts "#{' '*depth}CRAFT #{mul} #{count}"
+        puts "#{' '*depth}CRAFT #{mul} #{count}"
         recipe, tail = tree
-        # puts "#{' '*depth}RECIPE #{recipe}"
         @craft_seq[recipe] = [@craft_seq[recipe] || 0, depth].max
+        # ile potrzebuję?
+        if request_craft recipe, mul, count
+            tail.each do |num, rule|
+                process rule, depth+1, n
+            end
+        end
+    end
+
+    # jeśli craft potrzebny, dolicz go w odpowiedniej ilości i zwróć true
+    # jeśli niepotrzebny, zwróć false
+    def request_craft recipe, mul, count
+        needed = [exact_craft(mul * count, recipe.makes), count].max
+        if stash[recipe] > needed
+            stash[recipe] -= needed
+            return false
+        end
+        # potrzebujemy needed - stash[recipe] itemów, zaokrąglonych do możliwości craftowych
+    end
+
+
+    def obsolete_craft depth, mul, count, tree
+        puts "#{' '*depth}DOCRAFT #{mul} #{count}"
+        recipe, tail = tree
+        puts "#{' '*depth}RECIPE #{recipe}"
+        @craft_seq[recipe] = [@craft_seq[recipe] || 0, depth].max
+
         new_count = exact_craft(mul * count, recipe.makes)
-        # puts "#{' '*depth}NUM #{@crafts[recipe]} + #{count} or #{new_count}"
-        @crafts[recipe] += [count, new_count].max
+        puts "#{' '*depth}NUM #{@crafts[recipe]} + C(#{count}) or NC(#{new_count})"
+        added_count = [count, new_count].max #!
+        @crafts[recipe] += added_count
+        @craft_acc[recipe] += added_count
         tail.each do |num, rule|
             process rule, depth+1, new_count
         end
@@ -44,6 +76,7 @@ class Solver
     def describe
       show_raw
       show_crafts
+      show_stash
       nil
     end
 
@@ -58,6 +91,7 @@ class Solver
         ordering = craft_seq.sort_by { |craft, i| i }.reverse
         j, last = 1, ordering.first[1] + 1# max waga
         ordering.map do |craft, order|
+          # NOTE: poprawione o craft_acc
           count = crafts[craft]
           row = OpenStruct.new(
             count: (count * craft.makes).round,
@@ -92,6 +126,13 @@ class Solver
         last_num = row.num
         puts msg
       end
+    end
+
+    def show_acc
+        puts "Total crafted"
+        @craft_acc.each do |craft, count|
+            puts [craft.result, "*" , count].join(" ") 
+        end
     end
 
 end
