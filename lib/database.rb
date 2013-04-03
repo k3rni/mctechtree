3446f38b904fda88e2b61c4eab7fc03a912a1ccc
Krzysztof Zych <krzysztof.zych@connectmedica.com>
2013-04-03 14:28:26 +0200

    indentation

--- lib/database.rb
+++ lib/database.rb
@@ -39,13 +39,13 @@ class Database < Set
       definitions.each do |data|
         name, stacks, cost = parse_primitive(data)
         name = data.keys.first
-	if (old = find(name))
-	    # no need to flag it, identically named primitives are always equivalent
-	    # @conflicts.add [:primitive, name, group, old.group] 
-	    next
-	else
-    	    item = Item.primitive(name, cost, stacks, group)
-	end
+        if (old = find(name))
+          # no need to flag it, identically named primitives are always equivalent
+          # @conflicts.add [:primitive, name, group, old.group] 
+          next
+        else
+          item = Item.primitive(name, cost, stacks, group)
+        end
         self.add item
       end
     end
