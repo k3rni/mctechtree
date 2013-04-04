b0b20678547f8ac9bccf6e17d8821146cc1f70b1
Krzysztof Zych <krzysztof.zych@connectmedica.com>
2013-04-04 12:23:07 +0200

    Separate file with exceptions

---
+++ lib/errors.rb
@@ -0,0 +1,6 @@
+
+class UncraftableItemError < StandardError; end
+class UndefinedItemsError < StandardError; end
+class BadDefinitionError < StandardError; end
+class NamingConflictError < StandardError; end
+class DuplicatePrimitiveError < StandardError; end
