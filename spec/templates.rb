de423f140e89be58b02adf6e5db72fb3a98f5b7d
Krzysztof Zych <krzysztof.zych@connectmedica.com>
2013-05-09 16:17:28 +0200

    some tests

--- spec/templates.rb
+++ spec/templates.rb
@@ -15,19 +15,30 @@ describe Database do
     ingredients: [$(color) wool, stick*2]
     color: [R/red, O/orange, Y/yellow, G/green, B/blue, P/purple]  
   )
-  GEMS = 
+  TURTLES = %($(peripheral) $(tool) turtle:
+    ingredients: [P|$(peripheral), turtle, T|$(tool)]
+    shape: ~ ~ ~ T t P ~ ~ ~
+    tool: [~, mining/diamond pickaxe, farming/diamond hoe, melee/diamond sword, felling/diamond axe, digging/diamond shovel]
+    peripheral: [~, crafty/crafting table, wireless/wireless modem]
+  )
+
 
   context "templates" do
     it 'expands single-key recipe templates' do
       template = YAML.load PAINTBRUSH
       recipes = @db.transform_template template
       recipes.should have(6).items
-      recipes.map(&:name).should include('paintbrush (red)', 'paintbrush (purple)')
-      recipes.map(&:name).should_not include('painbrush (R)')
+      recipes.map{|r| r.keys.first}.should include('paintbrush (R)', 'paintbrush (P)')
+      recipes.map{|r| r.keys.first}.should_not include('paintbrush (red)') # wrong key expansion
     end
 
     it 'expands multi-key recipe templates' do
-      pending
+      template = YAML.load TURTLES
+      recipes = @db.transform_template template
+      recipes.should have(17).items
+      recipes.map{|r| r.keys.first}.should include('crafty melee turtle', 'wireless felling turtle', 'digging turtle', 'crafty turtle')
+      recipes.map{|r| r.keys.first}.should_not include('turtle') # all-nil combo
+      recipes.map{|r| r.keys.first}.should_not include('diamond hoe turtle', 'crafting table turtle') # wrong key expansion
     end
 
     it 'expands non-recipe templates' do
