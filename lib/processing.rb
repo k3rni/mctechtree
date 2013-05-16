d9172981b76ba7f896456772686ea63b50afa49d
Krzysztof Zych <krzysztof.zych@connectmedica.com>
2013-05-16 10:20:51 +0200

    templates in processing

--- lib/processing.rb
+++ lib/processing.rb
@@ -1,13 +1,28 @@
 module Processing
+  def is_process_template? process
+    process.include? 'vars'
+  end
+
   def load_processing definitions, group=nil
     definitions.each do |process|
-      inputs = process.delete('inputs')
-      outputs = process.delete('outputs')
-      if outputs.size == 1
-        load_single_craft craft_process(inputs, outputs, process), group
+      if is_process_template? process
+        transform_template(process).each do |p| 
+          # processes don't have a name, so they expand to {nil => {inputs=>...,outputs=>}}
+          load_single_process p.values.first, group
+        end
       else
-        raise
+        load_single_process process, group
       end
+    end
+  end
+
+  def load_single_process process, group
+    inputs = process.delete('inputs')
+    outputs = process.delete('outputs')
+    if outputs.size == 1
+      load_single_craft craft_process(inputs, outputs, process), group
+    else
+      raise
     end
   end
 
