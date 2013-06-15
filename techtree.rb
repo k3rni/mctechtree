6680fabce67378c5cd76330c718834f41a3915fb
Krzysztof Zych <k33rni@gmail.com>
2013-06-15 15:16:30 +0200

    optparse, cluster and filename skipping

--- techtree.rb
+++ techtree.rb
@@ -4,6 +4,8 @@
 require 'bundler'
 Bundler.setup
 Bundler.require
+require 'optparse'
+require 'ostruct'
 require 'delegate'
 require 'yaml'
 require 'set'
@@ -20,9 +22,32 @@ autoload :Solver, './lib/solver'
 autoload :Addons, './lib/addons'
 require './lib/errors'
 
+options = OpenStruct.new(skip: [], skip_clusters: [], skip_tiers: false)
+OptionParser.new do |opts|
+  opts.on('-X', '--exclude PATTERN', 'skip loading files by pattern') do |pattern|
+    options.skip << pattern
+  end 
+  opts.on('-x', '--exclude-tiers PATTERN', 'skip loading definitions by tier') do |pattern|
+    options.skip_clusters += pattern.split(',')
+  end
+  opts.on('--skip-tiers', 'omit tier calculation') do |v|
+    options.skip_tiers = v
+  end
+end.parse!
+
 DB = Database.new
 Dir.glob('db/**/*.yml').each do |filename|
-    DB.load_definitions YAML.load_file(filename)
+  if options.skip.any? { |pat| File.fnmatch? pat, filename }
+    puts "Skipping #{filename}"
+    next
+  end
+  definitions = YAML.load_file(filename)
+  if options.skip_clusters.include? definitions['cluster'] or options.skip_clusters.include? definitions['parent']
+    puts "Cluster-skipping #{filename}" 
+    next
+  end
+  puts "Loading #{filename}"
+  DB.load_definitions definitions
 end
 DB.fixup_pending
 DB.detect_name_clashes
